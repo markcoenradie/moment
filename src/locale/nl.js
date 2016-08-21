59260a49d353b7a21a654e4fdc0fd6763700e7de
markcoenradie <github@markcoenradie.nl>
2016-08-22 00:03:30 +0200

    Removed dots from weekdays
    No one would ever use that in Dutch

--- src/locale/nl.js
+++ src/locale/nl.js
@@ -18,7 +18,7 @@ export default moment.defineLocale('nl', {
     },
     monthsParseExact : true,
     weekdays : 'zondag_maandag_dinsdag_woensdag_donderdag_vrijdag_zaterdag'.split('_'),
-    weekdaysShort : 'zo._ma._di._wo._do._vr._za.'.split('_'),
+    weekdaysShort : 'zo_ma_di_wo_do_vr_za'.split('_'),
     weekdaysMin : 'Zo_Ma_Di_Wo_Do_Vr_Za'.split('_'),
     weekdaysParseExact : true,
     longDateFormat : {
